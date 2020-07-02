d56afbf939314d8276c2c0b96d33e6ec68385f87
xuke <kkhsu123@163.com>
2020-07-02 22:15:57 +0800

    2

--- projects/farris-dict/src/app/app-routing.module.ts
+++ projects/farris-dict/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
-    { path: 'dict', loadChildren: './dict/dict.module#DictModule' },
+    {
+        path: 'dict',
+        // loadChildren: './dict/dict.module#DictModule' ,
+        loadChildren: () =>
+            import('./dict/dict.module').then((m) => m.DictModule),
+    },
     { path: '**', redirectTo: 'dict' },
 ];
 @NgModule({
